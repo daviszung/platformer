e8c5756686e0f6766d50405400d183062036ad96
daviszung <daviszung@gmail.com>
2024-03-22 13:14:21 -0400

    toggle sidebar

--- editor.py
+++ editor.py
@@ -15,13 +15,14 @@ class Editor:
         self.SCALED_TILE_SIZE = self.BASE_TILE_SIZE * self.SCALING_FACTOR
         self.SCREEN_SIZE = (1600, 960)
         self.screen = pygame.display.set_mode(self.SCREEN_SIZE)
-        self.star_tileset = load_image("star_tileset.png")
         self.clock = pygame.time.Clock()
         self.last_time = time.time()
         self.font = pygame.Font("assets/fonts/monogram.ttf")
-        self.fps_display = self.font.render("FPS", False, pygame.Color("white"))
+        self.star_tileset = load_image("star_tileset.png")
         self.scroll: list[float] = [0, 0]
         self.selected_tile: None | Editor_Tile_Btn = None
+        self.sidebar_mode: str = "tiles"
+        self.sidebar_mode_toggle_btn = pygame.Rect(120, 56, 48, 20)
 
         self.bg = Bg(self.SCREEN_SIZE, load_images("background", True))
 
@@ -87,6 +88,9 @@ class Editor:
         self.map.render_to_surf(self.bg.current_bg, self.scroll)
 
     def run_main_loop(self):
+
+        clicked_prev = pygame.mouse.get_pressed()
+
         while 1:
 
             self.did_scroll = False
@@ -128,7 +132,9 @@ class Editor:
 
             clicked_curr = pygame.mouse.get_pressed()
             left_click = clicked_curr[0]
+            left_click_once = left_click and not clicked_prev[0]
             right_click = clicked_curr[2]
+            clicked_prev = clicked_curr
 
             # selecting tiles
             if mx <= 288 and left_click:
@@ -141,6 +147,12 @@ class Editor:
                     )
                     if rect.collidepoint(mx, my):
                         self.selected_tile = btn
+                
+                if self.sidebar_mode_toggle_btn.collidepoint(mx, my) and left_click_once:
+                    if self.sidebar_mode == "tiles":
+                        self.sidebar_mode = "props"
+                    elif self.sidebar_mode == "props":
+                        self.sidebar_mode = "tiles"
 
             if mx > 288 and left_click and self.selected_tile:
                 tile_x, tile_y = int(
@@ -175,8 +187,13 @@ class Editor:
                 self.screen, pygame.Color(45, 43, 85), (0, 0, self.SCREEN_SIZE[0], 48)
             )
 
-            for btn in self.editor_tile_btns:
-                btn.render(self.screen)
+            pygame.draw.rect(
+                self.screen, pygame.Color(80, 43, 85), self.sidebar_mode_toggle_btn
+            )
+
+            if self.sidebar_mode == "tiles":
+                for btn in self.editor_tile_btns:
+                    btn.render(self.screen)
 
             draw_text(
                 self.screen, f"{round(self.clock.get_fps())} FPS", [20, 20], self.font
@@ -187,17 +204,21 @@ class Editor:
             )
 
             draw_text(
+                self.screen, f"{self.sidebar_mode}", [124, 56], self.font
+            )
+
+            draw_text(
                 self.screen,
                 f"tile ({(mx + self.scroll[0]) // self.SCALED_TILE_SIZE}, {(my + self.scroll[1]) // self.SCALED_TILE_SIZE})",
                 [280, 20],
-                self.font,
+                self.font
             )
 
             draw_text(
                 self.screen,
                 f"scroll: {round(self.scroll[0]), round(self.scroll[1])}",
                 [460, 20],
-                self.font,
+                self.font
             )
 
             if mx > 288 and my > 48:
